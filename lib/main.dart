c85c923ede277442ccd2f2f4bcde471df8093555
bornaventure <anthonyuzonna341@gmail.com>
2022-07-02 17:25:49 +0100

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Chores',
       theme: ThemeData(
       ),
       home: const SplashPage(),
